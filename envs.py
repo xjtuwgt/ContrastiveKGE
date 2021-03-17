d924cbbe505da45547b26b1a59497d937dc700e5
guangtao.wang <guangtao.wang@jd.com>
2021-03-16 19:38:01 -0700

    json file

--- envs.py
+++ envs.py
@@ -3,13 +3,12 @@ import logging
 import sys
 from os.path import join
 PROJECT_FOLDER = os.path.dirname(__file__)
-print(PROJECT_FOLDER)
+# print(PROJECT_FOLDER)
 sys.path.append(join(PROJECT_FOLDER))
 
 # Define the dataset folder and model folder based on environment
-# HOME_DATA_FOLDER = '/ssd/HGN/data'
 HOME_DATA_FOLDER = join(PROJECT_FOLDER, 'data')
 KG_DATASET_FOLDER = join(PROJECT_FOLDER, 'data')
-COMMON_KG_DATASET_FOLDER = join(PROJECT_FOLDER, 'commonsensedata')
-CodEX_DATASET_FOLDER = join(PROJECT_FOLDER, 'codexdata')
+COMMON_KG_DATASET_FOLDER = join(KG_DATASET_FOLDER, 'commonsensedata')
+CodEX_DATASET_FOLDER = join(KG_DATASET_FOLDER, 'codexdata')
 OUTPUT_FOLDER = join(HOME_DATA_FOLDER, 'outputs')
